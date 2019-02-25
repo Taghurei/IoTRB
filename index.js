48f128e6fe3dd7c29d5c4caceb9eb834bc50bfb9
taghurei <gauthier.lacroix@supelec.fr>
2019-02-25 16:20:29 +0100

    'e'

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const app = express();
 const path = require('path');
 const router = express.Router();
 var fs = require('fs');
+const SerialPort = require('serialport')
+
+const Readline = SerialPort.parsers.Readline
+const port = new SerialPort("/dev/ttyACM0")
+const parser = new Readline()
+port.pipe(parser)
+parser.on('data', (data)=>{console.log(data)})
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
@@ -32,3 +39,11 @@ app.use('/', router);
 app.listen(process.env.port || 3000);
 
 console.log('Running at Port 3000');
+
+/*
+const Readline = require('@serialport/parser-readline')
+const serialPort= new SerialPort('/dev/ttyACM0')
+const parser= serialPort.pipe(new Readline({delimiter:'\n'}));
+parser.on('data', handleSerialPort);
+
+handleSerialPort*/
