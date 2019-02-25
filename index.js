8d1f5606e664e74eac8ff9d3bfba3e204b467bae
taghurei <gauthier.lacroix@supelec.fr>
2019-02-25 15:22:50 +0100

    json

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var bodyParser = require('body-parser')
+const express = require('express');
+const app = express();
+const path = require('path');
+const router = express.Router();
+var fs = require('fs');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(express.json())
+router.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/front.html'));
+    //__dirname : It will resolve to your project folder.
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+router.get('/json', function (req, res) {
+    res.sendFile(path.join(__dirname + '/test.json'));
 });
+
+
+
+router.post('/change', function(req,res){
+    console.log("postroute")
+    console.log(req.body)
+    fs.writeFileSync('test.json', JSON.stringify(req.body));
+    res.sendFile(path.join(__dirname + '/test.json'));
+})
+
+//add the router
+app.use('/', router);
+app.listen(process.env.port || 3000);
+
+console.log('Running at Port 3000');
